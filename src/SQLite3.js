06434ee2213b797f17c8f706947cb04d44d3e32c
justinwoo <moomoowoo@gmail.com>
2017-03-15 20:29:20 +0200

    update dependencies and build

--- src/SQLite3.js
+++ src/SQLite3.js
@@ -3,15 +3,15 @@ var sqlite3 = require('sqlite3');
 exports._newDB = function (filename, cb) {
   return function () {
     cb(new sqlite3.Database(filename))();
-  }
-}
+  };
+};
 
 exports._closeDB = function (db, cb) {
   return function () {
     db.close();
     cb()();
-  }
-}
+  };
+};
 
 exports._queryDB = function (db, query, params, cb) {
   return function () {
@@ -22,5 +22,5 @@ exports._queryDB = function (db, query, params, cb) {
         cb(rows)();
       }
     })));
-  }
-}
+  };
+};
