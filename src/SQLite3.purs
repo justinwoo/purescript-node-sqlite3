54e1910f0fd172662fb88152e174e27a57e33980
Felix Schlitter <felixschlitter@protonmail.com>
2018-05-21 15:00:43 +1200

    Avoid throw queryDB errors off-stack

--- src/SQLite3.purs
+++ src/SQLite3.purs
@@ -2,10 +2,11 @@ module SQLite3 where
 
 import Prelude
 
-import Control.Monad.Aff (Aff, makeAff)
+import Control.Monad.Aff (Aff, Error, makeAff)
 import Control.Monad.Eff (Eff, kind Effect)
+import Data.Either (Either(..))
 import Data.Foreign (Foreign)
-import Data.Function.Uncurried (runFn4, runFn2, Fn4, Fn2)
+import Data.Function.Uncurried (Fn2, Fn5, runFn2, runFn5)
 import Data.Monoid (mempty)
 
 type FilePath = String
@@ -20,25 +21,27 @@ foreign import _newDB :: forall e.
     FilePath
     (DBConnection -> Eff (db :: DBEffects | e) Unit)
   (Eff (db :: DBEffects | e) Unit)
+
 foreign import _closeDB :: forall e.
-  Fn2
-    DBConnection
-    (Unit -> Eff (db :: DBEffects | e) Unit)
+    DBConnection ->
     (Eff (db :: DBEffects | e) Unit)
+
 foreign import _queryDB :: forall e.
-  Fn4
+  Fn5
     DBConnection
     Query
     (Array Param)
+    (Error -> Eff (db :: DBEffects | e) Unit)
     (Foreign -> Eff (db :: DBEffects | e) Unit)
   (Eff (db :: DBEffects | e) Unit)
 
 newDB :: forall e. FilePath -> Aff (db :: DBEffects | e) DBConnection
 newDB path =
   makeAff \cb -> mempty <$ runFn2 _newDB path (cb <<< pure)
-closeDB :: forall e. DBConnection -> Aff (db :: DBEffects | e) Unit
-closeDB conn =
-  makeAff \cb -> mempty <$ runFn2 _closeDB conn (cb <<< pure)
+
+closeDB :: forall e. DBConnection -> Eff (db :: DBEffects | e) Unit
+closeDB conn = _closeDB conn
+
 queryDB :: forall e. DBConnection -> Query -> Array Param -> Aff (db :: DBEffects | e) Foreign
 queryDB conn query params =
-  makeAff \cb -> mempty <$ runFn4 _queryDB conn query params (cb <<< pure)
+  makeAff \cb -> mempty <$ runFn5 _queryDB conn query params (cb <<< Left) (cb <<< Right)
